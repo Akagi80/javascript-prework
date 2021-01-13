7acd46cde2ba74f181ef0d5a59fc2530366bd507
Sebastian Sikora <ssikora80@gmail.com>
2021-01-13 22:30:16 +0100

    Change named function to anonymous in script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 { 
-    function playGame(playerInput) {
+    const playGame = function(playerInput) {
         clearMessages();
 
-        function getMoveName(argMoveId) {
+        const getMoveName = function(argMoveId) {
             if (argMoveId == 1) {
                 return 'kamień';
             } else if (argMoveId == 2) {
@@ -15,7 +15,7 @@
             }
         }
 
-        function displayResult() {   
+        const displayResult = function() {   
             const randomNumber = Math.floor(Math.random() * 3 + 1),
                 argComputerMove = getMoveName(randomNumber),
                 argPlayerMove = getMoveName(playerInput);
